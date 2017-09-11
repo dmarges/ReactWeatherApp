59436f3201fac8183b46613c366d63796b58ae57
djm <donniemarges@gmail.com>
2017-09-11 12:14:08 -0700

    styled about page and added content to about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,25 @@ var React = require('react');
 
 var About = (props) => {
 	return (
-		<h3>About Page!</h3>
+		<div>
+			<h1 className="text-center">About</h1>
+			<p>
+				This is a simple Weather Application that I built using React and ES6.
+			</p>
+			<p>
+				Here are the tools that I used:
+			</p>
+			<ul>
+				<li>
+					<a href="https://facebook.github.io/react">React</a> - This was the JavaScript
+					Framework that I used.
+				</li>
+				<li>
+					<a href="http://openweathermap.org">Open Weather Map</a> - This is the Weather
+					API that I used to search for weather data by city name.
+				</li>
+			</ul>
+		</div>
 	);
 };
 
